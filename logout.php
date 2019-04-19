7eb598cf1201181e2aea9a0543d423499034bb13
leo0316 <leo0316@yahoo.com.tw>
2019-04-19 20:10:44 +0800

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,8 +1,27 @@
 <?php session_start(); ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<head>
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/css/mdb.min.css" rel="stylesheet">        
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/js/mdb.min.js"></script>
+</head>
 <?php
 //將session清空
 unset($_SESSION['username']);
-echo '登出中......';
+echo '<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>';
+echo '<div class="text-center">
+<div class="spinner-border" role="status">
+  <span class="sr-only">Loading...</span></div></div>';
 echo '<meta http-equiv=REFRESH CONTENT=1;url=index.html>';
 ?>
